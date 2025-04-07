e874761b8923916da4defd717a9b36ea4e9d07b6
Isabella <146090324+thnksisa@users.noreply.github.com>
2025-04-06 21:13:05 -0300

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,29 @@
+'''5. Construir um programa que leia um conjunto de caracteres (uma frase, terminada por
+“Enter”) da entrada padrão e ao final imprima o número de caracteres lidos.'''
+
+while True:
+
+    print("-" * 50)
+    print(" " * 19, "𝗖𝗢𝗡𝗧𝗔𝗗𝗢𝗥")
+    print("-" * 50)
+
+    frase = str(input("Insira uma frase e quando terminar. Digite “Enter”: \n"))
+    acumulador = 0
+
+    for char in frase:
+        acumulador += 1
+
+    print(f"Sua frase possui {acumulador} caracteres.")
+    print("-" * 50)
+    
+    continuar = str(input("Deseja continuar a escrever? (Sim/Não): \n")).lower()
+    
+    if continuar == "nao" or continuar == "não":
+        print("Obrigado por utilizar o programa.")
+        print("-" * 50)
+        break
+    elif continuar == "sim":
+        print("-" * 50)
+        continue
+    else:
+        break
